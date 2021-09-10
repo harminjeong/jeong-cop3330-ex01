79187e6896bd941b5b7373e2a078d36bd8fb5063
Harmin Jeong <harminjeong@gmail.com>
2021-09-09 20:57:39 -0400

    ex01

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,18 @@
 package org.example;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Harmin Jeong
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Harmin!" );
+        String name;
+        Scanner input = new Scanner(System.in);
+        System.out.println( "What is your name!");
+        name = input.nextLine();
+        System.out.printf("Hello, %s, nice to meet you!", name);
     }
 }
